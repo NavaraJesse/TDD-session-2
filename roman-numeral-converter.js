4415ac853629ae353acf0fe59147ef6110b7c424
Ronald Moeijes <ronald.moeijes@navara.nl>
2022-09-27 15:40:11 +0200

    Refactored while loop

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -1,17 +1,19 @@
 function convert(number) {
     let result = "";
     while(number > 0) {
-        if (number === 9) {
-            return "IX"
+        if (number >= 9) {
+            result = result + "IX"
+            number = number - 9
         }
         if (number >= 5) {
             result = result + "V";
             number = number - 5
         }
-        if (number === 4) {
-            return "IV"
+        if (number >= 4) {
+            result = result + "IV"
+            number = number - 4
         }
-        if (number > 0) {
+        if (number >= 1) {
             result = result + "I"
             number = number - 1
         }
